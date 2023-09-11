288bbad5ab3ef3441ffed0aa52cb21bcece89812
shivansh13579 <shivansh0975@gmail.com>
2023-09-11 16:30:03 +0530

    learn hooks in react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Cards from './components/Cards'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-green-600 text-black padding-4 rounded-xl mb-4'>shivam</h1>
+      <Cards/>
+    </>
+  )
+}
+
+export default App
